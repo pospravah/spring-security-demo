9ed697d7fdaf11f005b615ab8e35892a41045fa6
Stas T <atqcpospravah@gmail.com>
2018-08-21 15:01:21 +0300

    start point is running!

---
+++ spring-security-demo-01-base-app/src/main/java/com/edu/springsecutity/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,19 @@
+package com.edu.springsecutity.demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+	
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
